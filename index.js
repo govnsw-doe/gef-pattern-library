9d40a50f984bd368d2e8d623f7538d6584a23382
Alex Dewez-Lopez <alex.dewezlopez@det.nsw.edu.au>
2016-11-30 12:04:23 +1100

    (UPD) fixed a redirect

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ var metalsmith = Metalsmith(__dirname)
     pretty:     true
   }))
   .use(redirect({
-    '/': '/overview/welcome.html',
+    '/': 'overview/welcome.html',
   }))
   .use(prism())
   .use(bsync({
